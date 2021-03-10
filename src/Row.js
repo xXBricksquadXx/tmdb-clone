df7ca93e7d80f910acdce29a198ee2f38f9e9c5d
colby <colby88cardell@gmail.com>
2021-03-09 20:56:56 -0500

    :sparkles: rendered images

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import {useState,useEffect} from "react"
 import axios from "./axios";
 
+const base_url = "https://image.tmdb.org/t/p/original";
+
 function Row({title, fetchUrl }){
     const [movies, setMovies]= useState([])
 // a snippet of code which runs based on a specific condition/variable
@@ -8,17 +10,28 @@ function Row({title, fetchUrl }){
 // run once when the row loads and dont run again
         async function fetchData() {
             const request = await  axios.get(fetchUrl);
-            console.log(request)
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.log(movies)
 
     return(
-        <div>
+        <div className="row">
             <h2>{title}</h2>
 
-            {/* container -> posters */}
+            <div className="row_posters">
+                {/* several row_posters */}
+
+                {movies.map(movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+
+            </div>
+
+
 
 
         </div>

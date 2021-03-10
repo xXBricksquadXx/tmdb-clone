7040ec7cf339b135790425e0c70571ed5c3a7696
colby <colby88cardell@gmail.com>
2021-03-09 21:38:33 -0500

    Added Navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import requests from "./requests";
-import './App.css';
+import React from "react";
+import "./App.css";
 import Row from "./components/Row";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-<h1>Hello  its me</h1>
-        <Row title="Originals" fetchUrl={requests.fetchNetflixOriginals} />
-        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      {/* Navbar */}
+      <Nav />
+
+      {/* Banner */}
+      <Banner />
+
+      {/* Rows */}
+      <Row
+        title="ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }

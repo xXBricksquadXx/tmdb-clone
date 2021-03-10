81e343479a710116163674b5112c982ceee06075
colby <colby88cardell@gmail.com>
2021-03-09 20:21:25 -0500

    :construction: get beggining url and passed in 'fetchUrl'

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,17 @@
 import {useState,useEffect} from "react"
-
+import axios from "./axios";
 
 function Row({title, fetchUrl }){
     const [movies, setMovies]= useState([])
 // a snippet of code which runs based on a specific condition/variable
     useEffect(() => {
 // run once when the row loads and dont run again
+        async function fetchData() {
+            const request = await  axios.get(fetchUrl);
+            console.log(request)
+            return request;
+        }
+        fetchData();
     }, []);
 
     return(

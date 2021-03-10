cd4b079792a2cb2aaa49d61f9a82abbad60f1988
colby <colby88cardell@gmail.com>
2021-03-09 19:54:11 -0500

    import axios deps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import requests from "./requests";
 import './App.css';
 import Row from "./Row";
 
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
 <h1>Hello  its me</h1>
-        <Row title="Originals" />
-        <Row title="Trending Now" />
+        <Row title="Originals" fetchUrl={requests.fetchNetflixOriginals} />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+/** base url to make requests to the movie db */
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
 });
